36da76b3d88dc9012142e77ccf527110f89f9a2c
Netta <bhi084@myy.haaga-helia.fi>
2025-02-04 08:48:39 +0200

    API to remote branch

--- WeatherApp/screens/IndexScreen.js
+++ WeatherApp/screens/IndexScreen.js
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getWeatherData } from '../weatherapi';
 
 export default function IndexScreen() {
+  const [weatherData, setWeatherData] = useState("");
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+  const handleFetch = () => {
+    getWeatherData()
+      .then(data => console.log("Haettu säädata:", JSON.stringify(data, null, 2)))
+      .catch(error => console.error("Virhe haettaessa säätietoa:", error));
+  };
+
   return (
     <View style={styles.container}>
       <Text>Index</Text>
