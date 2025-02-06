f9a573af00ebc93932ade23a80d777e3f2dea4c0
Netta <bhi084@myy.haaga-helia.fi>
2025-02-06 11:20:17 +0200

    Fixed API fetch

--- WeatherApp/api.js
+++ WeatherApp/api.js
@@ -2,9 +2,11 @@ const lat = 60.1674881;
 const lon = 24.9427473;
 
 export function getWeatherInHelsinki() {
-    return fetch(`${process.env.CURRENT_WEATHER_DATA_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+    return fetch(`${process.env.EXPO_PUBLIC_API_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+    // HUOM. API_KEY täytyy laittaa selkokielisenä (ei muuttujana, vaan suoraan tekstinä), koska avain on salainen.
+    // HUOM. EI SAA PUSHATA GITHUBIIN SELLAISENA VERSIONA!!!!! vaan muuttujan kanssa
     .then(response => {
-        if(!rersponse.ok)
+        if(!response.ok)
             throw new Error("Error in fetch", response.statusText);
 
         return response.json();
--- WeatherApp/screens/IndexScreen.js
+++ WeatherApp/screens/IndexScreen.js
@@ -10,7 +10,7 @@ export default function IndexScreen() {
 
   useEffect(() => {
     handleFetch();
-  });
+  }, []);
 
   const handleFetch = () => {
     getWeatherInHelsinki()
