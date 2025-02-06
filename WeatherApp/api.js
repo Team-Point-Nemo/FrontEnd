872021dd01cf9efe24d09aa6194b1681d5e0762e
Netta <bhi084@myy.haaga-helia.fi>
2025-02-06 10:37:54 +0200

    Added current weather in Hel to index and made function to api

--- WeatherApp/api.js
+++ WeatherApp/api.js
@@ -1,1 +1,12 @@
-export function
+const lat = 60.1674881;
+const lon = 24.9427473;
+
+export function getWeatherInHelsinki() {
+    return fetch(`${process.env.CURRENT_WEATHER_DATA_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+    .then(response => {
+        if(!rersponse.ok)
+            throw new Error("Error in fetch", response.statusText);
+
+        return response.json();
+    });
+}
--- WeatherApp/screens/IndexScreen.js
+++ WeatherApp/screens/IndexScreen.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getWeatherInHelsinki } from '../api';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function IndexScreen() {
+  const [temperature, setTemperature] = useState(0);
+  const [feelsLike, setFeelsLike] = useState(0);
+
+  useEffect(() => {
+    handleFetch();
+  });
+
+  const handleFetch = () => {
+    getWeatherInHelsinki()
+    .then(data => {
+      setTemperature(Number(data.main.temp) - 273.15)
+      setFeelsLike(Number(data.main.feels_like) - 273.15)
+    })
+    .catch(err => console.error(err))
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Index</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Temperature: {temperature} celsius</Text>
+      <Text>Feels like: {feelsLike} celsius</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
