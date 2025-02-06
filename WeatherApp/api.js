50075db65f603c5a98947039facc63b078b3f3e7
Heidi Ahlgren <heidiahlgren@hotmail.com>
2025-02-06 11:23:53 +0200

    erge

--- WeatherApp/api.js
+++ WeatherApp/api.js
@@ -2,9 +2,9 @@ const lat = 60.1674881;
 const lon = 24.9427473;
 
 export function getWeatherInHelsinki() {
-    return fetch(`${process.env.CURRENT_WEATHER_DATA_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+    return fetch(`${process.env.EXPO_PUBLIC_API_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.EXPO_PUBLIC_API_KEY}`)
     .then(response => {
-        if(!rersponse.ok)
+        if(!response.ok)
             throw new Error("Error in fetch", response.statusText);
 
         return response.json();
--- WeatherApp/screens/IndexScreen.js
+++ WeatherApp/screens/IndexScreen.js
@@ -10,13 +10,14 @@ export default function IndexScreen() {
 
   useEffect(() => {
     handleFetch();
-  });
+  }, []);
 
   const handleFetch = () => {
     getWeatherInHelsinki()
     .then(data => {
-      setTemperature(Number(data.main.temp) - 273.15)
-      setFeelsLike(Number(data.main.feels_like) - 273.15)
+      setTemperature(Number(data.main.temp) - 273.15);
+      console.log(data);
+      setFeelsLike(Number(data.main.feels_like) - 273.15);
     })
     .catch(err => console.error(err))
   };
