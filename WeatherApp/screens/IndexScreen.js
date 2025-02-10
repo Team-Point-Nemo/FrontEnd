38ff4dea007e297ee391a9e0239032491a163ef3
Heidi Ahlgren <heidiahlgren@hotmail.com>
2025-02-10 15:10:53 +0200

    weather object

--- WeatherApp/screens/IndexScreen.js
+++ WeatherApp/screens/IndexScreen.js
@@ -6,6 +6,12 @@ import { getWeatherInHelsinki } from '../api';
 export default function IndexScreen() {
   const [temperature, setTemperature] = useState(0);
   const [feelsLike, setFeelsLike] = useState(0);
+  const [weather, setWeather] = useState({
+    main: {
+      temp: 1, feels_like: 2
+    },
+    wind: {speed: 4},
+  })
 
   useEffect(() => {
     handleFetch();
@@ -13,17 +19,17 @@ export default function IndexScreen() {
 
   const handleFetch = () => {
     getWeatherInHelsinki()
-    .then(data => {
-      setTemperature((Number(data.main.temp) - 273.15).toFixed(1));
-      setFeelsLike((Number(data.main.feels_like) - 273.15).toFixed(1));
-    })
+    .then(data => setWeather(data))
     .catch(err => console.error(err))
   };
   
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Temperature: {temperature} celsius</Text>
-      <Text>Feels like: {feelsLike} celsius</Text>
+      {/* <Text>Temperature: {temperature} celsius</Text>
+      <Text>Feels like: {feelsLike} celsius</Text> */}
+      <Text>Temperature: {(weather.main.temp - 273.15).toFixed(1)}</Text>
+      <Text>Feels like: {(weather.main.feels_like - 273.15).toFixed(1)}</Text>
+      <Text>Wind speed: {weather.wind.speed.toFixed(0)} m/s</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
