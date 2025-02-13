8cbb9c52ee0961c9bb9372253ef330746ca928c3
Netta <bhi084@myy.haaga-helia.fi>
2025-02-13 15:55:22 +0200

    UserLocation.js

---
+++ WeatherApp/components/Location/UserLocation.js
@@ -0,0 +1,24 @@
+import * as Location from 'expo-location';
+import { useEffect } from 'react';
+
+export default function UserLocation({ onLocationFetched }) {
+    useEffect(() => {
+        fetchLocation();
+    }, []);
+
+    const fetchLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync(); // Checks if user has grant permissions for location
+        if (status !== 'granted') {
+            Alert.alert('Location permissions denied')
+            return;
+        }
+
+        try {
+            let location = await Location.getCurrentPositionAsync({}); // Get location as object 'coords'
+            onLocationFetched(location); // Passes object to parent component
+        } catch (err) {
+            console.error("Error in fetching location: ", err);
+            Alert.alert('Error in fetching location');
+        }
+    };
+}
