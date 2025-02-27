3724e67754170f448a87f80831792930a394266d
Heidi Ahlgren <heidiahlgren@hotmail.com>
2025-02-27 08:58:37 +0200

    WEATHER_API_KEY (;

--- WeatherApp/api.js
+++ WeatherApp/api.js
@@ -2,7 +2,7 @@ const lat = 60.1674881;
 const lon = 24.9427473;
 
 export function getWeatherInHelsinki() {
-    return fetch(`${process.env.EXPO_PUBLIC_API_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3d87bae887c82a2c3312c81b6f321a20`)
+    return fetch(`${process.env.EXPO_PUBLIC_API_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&${process.env.WEATHER_API_KEY}`)
     // HUOM. API_KEY täytyy laittaa selkokielisenä (ei muuttujana, vaan suoraan tekstinä), koska avain on salainen.
     // HUOM. EI SAA PUSHATA GITHUBIIN SELLAISENA VERSIONA!!!!! vaan muuttujan kanssa
     .then(response => {
