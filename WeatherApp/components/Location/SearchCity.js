7e87cdf2f94d745269d6b74f4125e2ca6c57334a
Anna <bhi022@myy.haaga-helia.fi>
2025-03-07 19:44:49 +0200

    fixing search logic and moved display it to search screen for now

--- WeatherApp/components/Location/SearchCity.js
+++ WeatherApp/components/Location/SearchCity.js
@@ -1,34 +1,27 @@
 import React, { useState } from "react";
 import { SafeAreaView, TextInput, Button, StyleSheet } from "react-native";
-import { getWeatherData } from "../../weatherapi";
-import WeatherNow from "../weather/WeatherNow";
+import { getWeatherByCity } from "../../api";
 
 export default function WeatherSearch() {
 
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const fetchWeatherData = async (city) => {
+  
+  const handleSearch = async () => {
+    if (!city.trim().length > 0) {
+      console.log("Error in city input" + error)
+      Alert.alert("Error in city input" + error)
+      return;
+    }
     try {
-      const data = await getWeatherData(city);
+      const data = await getWeatherByCity(city)
+      console.log("Weather data ", data)
       setWeather(data)
     } catch (error) {
-      console.error("Error fetching city " + error);
-    } finally {
-      setLoading(false);
+      console.log("Error in fetching weather data" + error)
+      Alert.alert("Error in fetching weather data" + error)
     }
-  };
-
-  const handleSearch = () => {
-    if (city.trim().length > 0) {
-      fetchWeatherData(city);
-      navigator.navigate('WeatherNow', weather)
-    } else {
-      console.log("Error in city input" + error)
-      Alert.alert("Input error")
-    }
-  };
+  }
 
   return (
     <SafeAreaView>
@@ -38,12 +31,24 @@ export default function WeatherSearch() {
         value={city}
         onChangeText={setCity}
       />
-      <Button 
-        title="Search" 
-        onPress={handleSearch} 
+      <Button
+        title="Search"
+        onPress={handleSearch}
         color='#808080'
+      />
+      { weather && (
+        <>
+        <Text>Temperature: {(weather.main.temp - 273.15).toFixed(0)} °C </Text>
+        <Text>Feels like: {(weather.main.feels_like - 273.15).toFixed(0)} °C</Text>
+        <Text>Wind speed: {weather.wind.speed.toFixed(0)} m/s</Text>
+        <Image
+          style={styles.weatherIcon}
+          source={{
+            uri: weather.weather ? `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png` : ''
+          }} 
         />
-      {weather && <WeatherNow weather={weather} />}
+        </>
+      )}
     </SafeAreaView>
   );
 }
@@ -56,9 +61,15 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
   },
-})
-
-/*
-lista kaupungeista -> muuttaa koordinaateiksi mitä käyttää tässä
-navigointi takaisin index, muotoiluhaku weathernow tiedostosta
-*/
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  weatherIcon: {
+    width: 70,
+    height: 70,
+    marginTop: 20,
+  },
+});
--- WeatherApp/screens/IndexScreen.js
+++ WeatherApp/screens/IndexScreen.js
@@ -1,11 +1,11 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
-import WeatherNow from '../components/weather/WeatherNow';
+import { StyleSheet, SafeAreaView } from "react-native";
+import WeatherNow from "../components/weather/WeatherNow";
 
 export default function IndexScreen() {
-  
+
   return (
     <SafeAreaView style={styles.container}>
-     <WeatherNow />
+      <WeatherNow />
     </SafeAreaView>
   );
 }
@@ -13,13 +13,8 @@ export default function IndexScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  weatherIcon: {
-    width: 70,
-    height: 70,
-    marginTop: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
