ae685ac5ee7a3c076e8a63fb6a100d8b54c2fc42
Netta <bhi084@myy.haaga-helia.fi>
2025-03-07 19:12:07 +0200

    Get current weather by user's location done. Also did some formatting to code.

--- WeatherApp/components/weather/WeatherNow.js
+++ WeatherApp/components/weather/WeatherNow.js
@@ -1,49 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
-import { getWeatherInHelsinki } from '../../api';
+import { getCurrentWeatherInLocation } from '../../api';
 import UserLocation from '../Location/UserLocation';
 import CityFetch from '../Location/CityFetch';
 
 export default function WeatherNow(){
-
-// const [city, setCity] = useState(null);
-const [location, setLocation] = useState(null);
-
-const handleLocationFetched = (location) => { // 'location'-object is passed from UserLocation-component
-  setLocation(location);
-}
-
-const [weather, setWeather] = useState({
-  main: {
-    temp: 280, feels_like: 282
-  },
-  wind: {speed: 4},
-})
+const [location, setLocation] = useState({});
+const [weather, setWeather] = useState([]);
 
 useEffect(() => {
-  handleFetch();
-}, []);
+  if (location.coords) {  // Ensures, that location.coords has a value
+    getWeather(location)
+  }
+}, [location]);
 
-const handleFetch = () => {
-  getWeatherInHelsinki()
-  .then(data => setWeather(data))
-  .catch(err => console.error(err))
+  const handleLocationFetched = (location) => {   // 'location'-object is passed from UserLocation-component
+  setLocation(location);
+  };
+
+const getWeather = async () => {
+  try {
+    const data = await getCurrentWeatherInLocation(location);
+    if (data) {
+      setWeather(data);
+    } else {
+      console.error("Weather data not found.");
+      setWeather(null);
+    }
+  } catch (err) {
+      console.error("Error in fetching location: ", err);
+  }
 };
 
 return (
   <SafeAreaView style={styles.container}>
-    <UserLocation onLocationFetched={handleLocationFetched} />
+  <UserLocation onLocationFetched={handleLocationFetched} />
     {location && <CityFetch location={location} />}
-    <Text>Temperature: {(weather.main.temp - 273.15).toFixed(0)} °C</Text>
-    <Text>Feels like: {(weather.main.feels_like - 273.15).toFixed(0)} °C</Text>
-    <Text>Wind speed: {weather.wind.speed.toFixed(0)} m/s</Text>
+    {weather?.main && (   // Checks, that weather (and main-array in it's data) has value, before rendering.
+    <>
+      <Text>Temperature: {(weather.main.temp - 273.15).toFixed(0)} °C</Text>
+      <Text>Feels like: {(weather.main.feels_like - 273.15).toFixed(0)} °C</Text>
+      <Text>Wind speed: {weather.wind.speed.toFixed(0)} m/s</Text>
+    </>
+  )}
+  {weather?.weather && weather.weather[0] ? (
     <Image 
       style={styles.weatherIcon}
-      source={{
-        uri: weather.weather ? `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png` : ''
-      }}
+      source={{ uri: `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png` }}
     />
+  ) : (
+    <Text>No weather icon available</Text>
+  )}
     <StatusBar style="auto" />
   </SafeAreaView>
 );
