e7085e6fe2a81f35ce1d4301305a1ec4a62e5043
Anna <bhi022@myy.haaga-helia.fi>
2025-04-07 14:03:53 +0300

    merged with newest paperUI

--- WeatherApp/tests/CityFetch.test.js
+++ WeatherApp/tests/CityFetch.test.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import * as Location from 'expo-location';
+import { render, waitFor } from '@testing-library/react-native';
 
+jest.mock('expo-location')
 
+test('gets right city from coordinates outside of city center', async () => {
+    const mockLocation = {
+        latitude: 60.4193,
+        longitude: 22.2315,
+    };
+
+});
