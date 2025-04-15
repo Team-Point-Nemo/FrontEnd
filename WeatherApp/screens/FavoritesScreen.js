abd70dfa0f9a98cd9389901524769eb89d530ecd
Tiriele <147039063+Tiriele@users.noreply.github.com>
2025-04-15 09:35:33 +0300

    First button created

--- WeatherApp/screens/FavoritesScreen.js
+++ WeatherApp/screens/FavoritesScreen.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import { View } from 'react-native'
-import { Text } from 'react-native-paper'
+import { View, StyleSheet } from 'react-native'
+import { Text, Button } from 'react-native-paper'
 
 export default function FavoritesScreen() {
+
+  const handleCityPress = (city) => {
+    console.log(`City pressed: ${city}`)
+  };
+
   return (
-   <View>
-    <Text>Favorites</Text>
+   <View style={styles.container}>
+    <Text variant="headlineMedium" style={styles.title}>Favorites</Text>
+    <Button
+      mode="contained"
+      onPress={() => handleCityPress('Helsinki')}
+      style={styles.button}
+      contentStyle={styles.buttonContent}
+      labelStyle={ styles.buttonLabel}
+      >
+      Helsinki
+    </Button>
    </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  title: {
+    marginBottom: 20,
+    color: '#333',
+  },
+  button: {
+    marginVertical: 10,
+    width: '50%',
+    height:'10%'
+  },
+  buttonContent: {
+    height: '100%',
+    justifyContent: 'center',
+  },
+  buttonLabel: {
+    fontSize: 20,
+    color: '#fff',
+  },
+});
