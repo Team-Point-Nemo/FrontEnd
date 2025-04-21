8ad9b7534505b91ccd98c7b4c1a77d20135e0681
Anna <bhi022@myy.haaga-helia.fi>
2025-04-21 23:51:54 +0300

    recent city search for new hooks

--- WeatherApp/components/weather/WeatherIndex.js
+++ WeatherApp/components/weather/WeatherIndex.js
@@ -3,10 +3,11 @@ import useWeather from "../../hooks/useWeather";
 import useUserLocation from "../../hooks/useUserLocation";
 import useCitySearch from "../../hooks/useCitySearch";
 import useCityName from "../../hooks/useCityName";
+import useRecentSearch from "../../hooks/useRecentSearch";
 import Forecast from './forecast/Forecast';
 import { getCurrentDate, setImageByTime } from "../date/DateService";
 import { Text, Searchbar, FAB } from "react-native-paper";
-import { StyleSheet, SafeAreaView, Image, View, ImageBackground } from "react-native";
+import { StyleSheet, SafeAreaView, Image, View, ImageBackground, TouchableOpacity, TouchableWithoutFeedback, ScrollView } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 
@@ -18,6 +19,7 @@ export default function NewWeatherNow() {
   const [searchQuery, setSearchQuery] = useState("");
   const [mode, setMode] = useState("user");
   const [searchLoading, setSearchLoading] = useState(false);
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
 
   const location = mode == "user" ? userLocation : searchLocation;
 
@@ -25,82 +27,111 @@ export default function NewWeatherNow() {
 
   const { city } = useCityName(location);
 
+  const { recentCities, updateRecentCities, dismisCityList } = useRecentSearch();
 
   const handleSearch = async () => {
     await searchCity(searchQuery); // Calls the hook to get the coordinates
+    await updateRecentCities(searchQuery)
     setMode("search")
     // setSearch(true); tsekkaa nää viel!
     setSearchQuery("");
   };
 
+  const handleRecentCitySelect = async () => {
+    await searchCity(city)
+    await updateRecentCities(city)
+    setMode("search")
+    setSearchQuery("")
+    setIsSearchFocused(false)
+  }
+
+
   return (
-    <View>
-      <View style={styles.flexContainer1}>
-        <ImageBackground
-          source={setImageByTime()}
-          style={styles.image}>
+    <View style={{ flex: 1 }}>
+      <TouchableWithoutFeedback onPress={dismisCityList}>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <View style={styles.flexContainer1}>
+            <ImageBackground
+              source={setImageByTime()}
+              style={styles.image}>
 
-          <SafeAreaView style={styles.image}>
-            <View style={styles.cityContainer}>
-              <View style={styles.cityLeft}>
-                {city ? (
-                  <Text variant="displayMedium" style={styles.textWithShadow}>{city}</Text>
-                  ) : (
-                  <ActivityIndicator animating={true} size="large" color={MD2Colors.black} />
-                  )}
-                <Text variant="titleLarge" style={styles.textWithShadow}>{getCurrentDate()}</Text>
-              </View>
-              <View style={styles.cityRigth}>
-                <FAB
-                  variant='surface'
-                  icon='map-marker'
-                  size={10}
-                  style={styles.fab}
-                  onPress={() => {
-                    setMode("user");
-                  }}
-                />
-              </View>
-            </View>
-
-            {weather?.main && (   // Checks, that weather has values before rendering.
-              <View style={styles.weatherContainer}>
-                <View style={styles.columnLeft}>
-                  <Text variant="displayLarge" style={styles.textWithShadow}>{Math.round(weather.main.temp)}°</Text>
-                  {weather?.weather?.icon ? (
-                    <Image
-                      style={styles.weatherIcon}
-                      source={{ uri: `http://openweathermap.org/img/wn/${weather.weather.icon}.png` }}
+              <SafeAreaView style={styles.image}>
+                <View style={styles.cityContainer}>
+                  <View style={styles.cityLeft}>
+                    {city ? (
+                      <Text variant="displayMedium" style={styles.textWithShadow}>{city}</Text>
+                    ) : (
+                      <ActivityIndicator animating={true} size="large" color={MD2Colors.black} />
+                    )}
+                    <Text variant="titleLarge" style={styles.textWithShadow}>{getCurrentDate()}</Text>
+                  </View>
+                  <View style={styles.cityRigth}>
+                    <FAB
+                      variant='surface'
+                      icon='map-marker'
+                      size={10}
+                      style={styles.fab}
+                      onPress={() => {
+                        setMode("user");
+                      }}
                     />
-                  ) : null}
+                  </View>
                 </View>
-                <View style={styles.columnRight}>
-                  <Text variant="titleMedium" style={styles.textWithShadow}>Feels like: {Math.round(weather.main.feels_like)}°</Text>
-                  <Text variant="titleMedium" style={styles.textWithShadow}>Wind speed: {Math.round(weather.wind.speed)} m/s</Text>
-                </View>
+
+                {weather?.main && (   // Checks, that weather has values before rendering.
+                  <View style={styles.weatherContainer}>
+                    <View style={styles.columnLeft}>
+                      <Text variant="displayLarge" style={styles.textWithShadow}>{Math.round(weather.main.temp)}°</Text>
+                      {weather?.weather?.icon ? (
+                        <Image
+                          style={styles.weatherIcon}
+                          source={{ uri: `http://openweathermap.org/img/wn/${weather.weather.icon}.png` }}
+                        />
+                      ) : null}
+                    </View>
+                    <View style={styles.columnRight}>
+                      <Text variant="titleMedium" style={styles.textWithShadow}>Feels like: {Math.round(weather.main.feels_like)}°</Text>
+                      <Text variant="titleMedium" style={styles.textWithShadow}>Wind speed: {Math.round(weather.wind.speed)} m/s</Text>
+                    </View>
+                  </View>
+                )}
+              </SafeAreaView>
+            </ImageBackground>
+
+            <View style={styles.locationContainer}>
+              <Searchbar
+                loading={searchLoading}
+                inputStyle={{ fontSize: 14 }}
+                elevation={3}
+                placeholder="Search city..."
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+                onIconPress={handleSearch}
+                onSubmitEditing={handleSearch}
+                style={styles.searchbar}
+                onFocus={() => setIsSearchFocused(true)}
+                onBlur={() => setIsSearchFocused(false)}
+              />
+            </View>
+            {isSearchFocused && recentCities.length > 0 && (
+              <View style={styles.recentCitiesList}>
+                {recentCities.map((item, index) => (
+                  <TouchableOpacity
+                    key={index}
+                    style={styles.recentCityItem}
+                    onPress={() => handleRecentCitySelect(item)}
+                  >
+                  <Text style={styles.recentCityText}>{item}</Text>
+                  </TouchableOpacity>
+                ))}
               </View>
             )}
-          </SafeAreaView>
-        </ImageBackground>
-
-        <View style={styles.locationContainer}>
-          <Searchbar
-            loading={searchLoading}
-            inputStyle={{ fontSize: 14 }}
-            elevation={3}
-            placeholder="Search city..."
-            onChangeText={setSearchQuery}
-            value={searchQuery}
-            onIconPress={handleSearch}
-            onSubmitEditing={handleSearch}
-            style={styles.searchbar}
-          />
-        </View>
-
-      </View>
-      <View style={styles.flexContainer2}>
-        <Forecast location={location} />
-      </View>
+          </View>
+          <View style={styles.flexContainer2}>
+            <Forecast location={location} />
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
@@ -144,8 +175,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   fab: {
-    width: '50',
-    height: '50',
+    width: 50,
+    height: 50,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -180,5 +211,21 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     marginHorizontal: 30,
     marginTop: 20,
+  },
+  recentCitiesList: {
+    marginTop: 10,
+    marginHorizontal: 30,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingVertical: 10,
+  },
+  recentCityItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  recentCityText: {
+    fontSize: 16,
+    color: '#333',
   },
 });
---
+++ WeatherApp/hooks/useRecentSearch.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Keyboard } from 'react-native';
+
+export default function useRecentSearch() {
+
+    const [recentCities, setRecentCities] = useState([]);
+
+    useEffect(() => {
+        const loadRecentCities = async () => {
+            try {
+                const savedCities = await AsyncStorage.getItem('recentCities');
+                if (savedCities) {
+                    setRecentCities(JSON.parse(savedCities));
+                }
+            } catch (err) {
+                console.error("Error loading recent cities from AsyncStorage:", err);
+            }
+        };
+        loadRecentCities();
+    }, []);
+
+
+    const updateRecentCities = async (city) => {
+        try {
+            const updatedCities = [city, ...recentCities.filter(c => c !== city)].slice(0, 5);
+            setRecentCities(updatedCities);
+            await AsyncStorage.setItem('recentCities', JSON.stringify(updatedCities));
+            return updatedCities
+        } catch (err) {
+            console.log("Error updating recent cities from AsyncStorage: ", err)
+        }
+    }
+
+    const dismisCityList = () => {
+        setIsSearchFocused(false);
+        Keyboard.dismiss();
+    }
+
+    return { recentCities, updateRecentCities, dismisCityList }
+
+} 
